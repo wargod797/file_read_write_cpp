635051271c5603d05f277bfb5bd3d3fed9853472
wargod797 <43702120+wargod797@users.noreply.github.com>
2019-12-19 00:02:39 +0530

    Add files via upload

---
+++ file_io.cpp
@@ -0,0 +1,36 @@
+//C++ program to write and read text in/from file.
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+   fstream file;
+   file.open("sample.txt",ios::out);
+   if(!file)
+   {
+       cout<<"Error in creating file!!!"<<endl;
+       return 0;
+   }
+   cout<<"File created successfully."<<endl;
+   file<<"ABCD.";
+   file.close();
+   file.open("sample.txt",ios::in);
+   if(!file)
+   {
+       cout<<"Error in opening file!!!"<<endl;
+       return 0;
+   }
+   char ch;
+   cout<<"File content: ";
+
+   while(!file.eof())
+   {
+       file>>ch;
+       cout<<ch;
+   }
+
+   file.close();
+   return 0;
+}
